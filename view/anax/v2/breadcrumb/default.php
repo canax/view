e49144ad06f294b996c88a44147d6df51105d943
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-10-18 17:01:14 +0200

    Move views to v2

--- view/anax/v2/breadcrumb/default.php
+++ view/anax/v2/breadcrumb/default.php
@@ -9,13 +9,6 @@ namespace Anax\View;
 // Show incoming variables and view helper functions
 //echo showEnvironment(get_defined_vars(), get_defined_functions());
 
-// Prepare classes
-$classes[] = "article";
-if (isset($class)) {
-    $classes[] = $class;
-}
-
-
-?><article <?= classList($classes) ?>>
-<?= $content ?>
-</article>
+renderView("anax/v2/link-list/default", [
+    "links" => $breadcrumb
+]);
